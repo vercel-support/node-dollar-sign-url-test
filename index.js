e2f38bfa460a30623567a92f4010984cb8b328a7
William Li <williamli@bbi.io>
2019-05-23 19:09:33 +0800

    $ routes demo

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(helmet())
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
+        <pre>${req.url}</pre>
+        <pre>${decodeURIComponent(req.url)}</pre>
     `)
 })
 
